9904a1090db566f86df44f5582079b1ea9c04b79
Dinma-Faith <phaytep@gmail.com>
2022-05-11 20:30:40 +0100

    Fix eslint errors

--- src/api.js
+++ src/api.js
@@ -2,20 +2,20 @@ const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/game
 const gameId = 'DOHDqsWYkGmCuwVtm0LV';
 
 export const postScores = async (name, score) => {
-    await fetch(`${url}${gameId}/scores`, {
-      method: 'POST',
-      body: JSON.stringify({
-        user: name,
-        score: score,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    }).then((result) => result.json());
-  };
+  await fetch(`${url}${gameId}/scores`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name,
+      score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((result) => result.json());
+};
 
 export const getScore = async () => {
-    const data = await fetch(`${url}${gameId}/scores`);
-    const inputData = await data.json();
-    return inputData;
-  };
+  const data = await fetch(`${url}${gameId}/scores`);
+  const inputData = await data.json();
+  return inputData;
+};
--- src/index.js
+++ src/index.js
@@ -8,27 +8,27 @@ const number = document.querySelector('#number');
 const refreshBtn = document.querySelector('#refresh');
 
 const createScore = async () => {
-    const data = await getScore();
-    const dataInfo = data.result;
-    renderPage.innerHTML = '';
-    dataInfo.forEach((record) => {
-      renderPage.innerHTML += `<li>${record.user}: ${record.score}</li>`;
-    });
-  };
-
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const myname = name.value;
-    const myNumber = number.value;
-    await postScores(myname, myNumber)
-    form.reset();
-    createScore();
+  const data = await getScore();
+  const dataInfo = data.result;
+  renderPage.innerHTML = '';
+  dataInfo.forEach((record) => {
+    renderPage.innerHTML += `<li>${record.user}: ${record.score}</li>`;
   });
+};
 
-  refreshBtn.addEventListener('click', async () => {
-    createScore();
-  });
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const myname = name.value;
+  const myNumber = number.value;
+  await postScores(myname, myNumber);
+  form.reset();
+  createScore();
+});
+
+refreshBtn.addEventListener('click', async () => {
+  createScore();
+});
   
-  window.addEventListener('load', () => {
-    createScore();
-  });
+window.addEventListener('load', () => {
+  createScore();
+});
