4a7536b85c88218f2e0cd829371fafef9e94aa19
Dinma-Faith <phaytep@gmail.com>
2022-05-11 20:08:43 +0100

    Use async and await functionalities to consume API

---
+++ src/api.js
@@ -0,0 +1,21 @@
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const gameId = 'DOHDqsWYkGmCuwVtm0LV';
+
+export const postScores = async (name, score) => {
+    await fetch(`${url}${gameId}/scores`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: name,
+        score: score,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }).then((result) => result.json());
+  };
+
+export const getScore = async () => {
+    const data = await fetch(`${url}${gameId}/scores`);
+    const inputData = await data.json();
+    return inputData;
+  };
