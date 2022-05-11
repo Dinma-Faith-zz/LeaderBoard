fea88c06b58ddf6d3d0c136e21167df5865077d2
Dinma-Faith <phaytep@gmail.com>
2022-05-11 20:36:46 +0100

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ form.addEventListener('submit', async (e) => {
 refreshBtn.addEventListener('click', async () => {
   createScore();
 });
- 
+
 window.addEventListener('load', () => {
   createScore();
 });
